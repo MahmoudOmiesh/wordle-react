869c9d6d3b682a358bba14c5e84d5382901a72c7
Mahmoud Omiesh <mahmoudomiesh2005@gmail.com>
2022-03-30 13:28:20 +0200

    initialized app

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<header className='header'>
+				<h1 className='header__title'>Wordle</h1>
+			</header>
+			<main className='main'></main>
+		</>
+	);
+}
+
+export default App;
